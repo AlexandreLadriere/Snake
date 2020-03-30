749f090d21861920b6afdf3825ce167e13bcef1f
AlexandreLadriere <alexandre.ladriere77@gmail.com>
2020-03-30 17:24:13 +0200

    Add: Adding Restart option

--- src/Constants.java
+++ src/Constants.java
@@ -7,5 +7,5 @@ public final class Constants {
     public static final int DOT_SIZE = 10; // Dot size
     public static final int MAX_DOTS = (B_WIDTH * B_HEIGHT) / (DOT_SIZE * DOT_SIZE); // Maximum number of possible dots on the board
     public static final int RAND_POS = 29; // Used to compute the random position of an "apple"
-    public static final int DELAY = 140; // Speed of the game
+    public static final int DELAY = 100; // Speed of the game
 }
